c03ba97290fddd0d2360844ff7578ca1a2e4b4eb
madvicspar <vmakkuzina@edu.hse.ru>
2024-01-19 16:23:01 +0500

    Added Utilities

---
+++ Cats-Cafe-Accounting-System/Utilities/Button.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace Cats_Cafe_Accounting_System.Utilities
+{
+    public class Button : RadioButton
+    {
+        static Button()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Button), new FrameworkPropertyMetadata(typeof(Button)));
+        }
+    }
+}
