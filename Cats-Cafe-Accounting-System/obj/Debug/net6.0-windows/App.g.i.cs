797e6d09234bd979a619c84824a8aeb113743373
madvicspar <vmakkuzina@edu.hse.ru>
2024-01-19 16:28:51 +0500

    Updated MainWindow

--- Cats-Cafe-Accounting-System/obj/Debug/net6.0-windows/App.g.i.cs
+++ Cats-Cafe-Accounting-System/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Cats_Cafe_Accounting_System {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Cats-Cafe-Accounting-System;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Cats-Cafe-Accounting-System;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
