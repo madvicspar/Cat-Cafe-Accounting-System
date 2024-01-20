97ebff1cbcbc10568f8a48b02e7355413d246114
madvicspar <vmakkuzina@edu.hse.ru>
2024-01-21 04:31:26 +0500

    Added getting petTransferLogEntries data grom database

--- Cats-Cafe-Accounting-System/Models/PetTransferLogEntryModel.cs
+++ Cats-Cafe-Accounting-System/Models/PetTransferLogEntryModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Cats_Cafe_Accounting_System.Utilities;
+using Google.Protobuf.WellKnownTypes;
+using System.Data;
 
 namespace Cats_Cafe_Accounting_System.Models
 {
@@ -14,6 +17,25 @@ namespace Cats_Cafe_Accounting_System.Models
         [ForeignKey("Pet")]
         public int PetId { get; set; }
         public PetModel Pet { get; set; }
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
+        public PetTransferLogEntryModel(int id, DateTime date, int visitorId, int petId, VisitorModel visitor, PetModel pet)
+        {
+            Id = id;
+            Date = date;
+            VisitorId = visitorId;
+            PetId = petId;
+            Visitor = visitor;
+            Pet = pet;
+        }
+        public PetTransferLogEntryModel(int id)
+        {
+            DataRow row = DBContext.GetById("owner_log", id);
+            Id = id;
+            Date = DateTime.Parse(row["date"].ToString());
+            VisitorId = Convert.ToInt32(row["visitor_id"]);
+            PetId = Convert.ToInt32(row["pet_id"]);
+            Visitor = new VisitorModel(VisitorId);
+            Pet = new PetModel(PetId);
+        }
     }
 }
--- Cats-Cafe-Accounting-System/ViewModels/PetTransferLogViewModel.cs
+++ Cats-Cafe-Accounting-System/ViewModels/PetTransferLogViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Cats_Cafe_Accounting_System.Models;
+using Cats_Cafe_Accounting_System.Utilities;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.ObjectModel;
+using System.Data;
 
 namespace Cats_Cafe_Accounting_System.ViewModels
 {
@@ -20,11 +22,23 @@ namespace Cats_Cafe_Accounting_System.ViewModels
         public PetTransferLogViewModel()
         {
             // Инициализация коллекции питомцев
-            PetTransferLogEntries = new ObservableCollection<PetTransferLogEntryModel>();
-            PetTransferLogEntries.Add(new PetTransferLogEntryModel { VisitorId = 1, PetId = 2, Date = new DateOnly(2023, 05, 23) });
-            PetTransferLogEntries.Add(new PetTransferLogEntryModel { VisitorId = 3, PetId = 8, Date = new DateOnly(2023, 02, 01) });
-            PetTransferLogEntries.Add(new PetTransferLogEntryModel { VisitorId = 4, PetId = 1, Date = new DateOnly(2023, 11, 19) });
+            PetTransferLogEntries = GetPetTransferLogEntriesFromTable("owner_log");
+        }
+        public static ObservableCollection<PetTransferLogEntryModel> GetPetTransferLogEntriesFromTable(string table)
+        {
+            ObservableCollection<PetTransferLogEntryModel> petTransferLogEntries = new ObservableCollection<PetTransferLogEntryModel>();
 
+            DataTable dataTable = DBContext.GetTable(table);
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                PetTransferLogEntryModel petTransferLogEntry = new PetTransferLogEntryModel(Convert.ToInt32(row["id_entry"]), DateTime.Parse(row["date"].ToString()),
+                    Convert.ToInt32(row["visitor_id"]), Convert.ToInt32(row["pet_id"]),
+                    new VisitorModel(Convert.ToInt32(row["visitor_id"])), new PetModel(Convert.ToInt32(row["pet_id"])));
+                petTransferLogEntries.Add(petTransferLogEntry);
+            }
+
+            return petTransferLogEntries;
         }
     }
 }
