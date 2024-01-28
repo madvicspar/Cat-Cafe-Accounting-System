b6476cd431e78c15cb9f2e6fc34cdf0d61a91689
madvicspar <vmakkuzina@edu.hse.ru>
2024-01-28 10:53:25 +0500

    Added 'buttons' and added export to Word and to Excel, but just in petsTable as example

--- Cats-Cafe-Accounting-System/ViewModels/PetsViewModel.cs
+++ Cats-Cafe-Accounting-System/ViewModels/PetsViewModel.cs
@@ -1,10 +1,17 @@
 ﻿using Cats_Cafe_Accounting_System.Models;
 using Cats_Cafe_Accounting_System.RegularClasses;
 using Cats_Cafe_Accounting_System.Utilities;
+using ClosedXML.Excel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using NPOI.XWPF.UserModel;
 using System;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Input;
 
 namespace Cats_Cafe_Accounting_System.ViewModels
 {
@@ -20,11 +27,86 @@ namespace Cats_Cafe_Accounting_System.ViewModels
                 OnPropertyChanged(nameof(Pets));
             }
         }
+        public ICommand ExcelExportCommand { get; set; }
+        public ICommand WordExportCommand { get; set; }
         public PetsViewModel()
         {
             // Инициализация коллекции питомцев
             Pets = GetPetsFromTable("pets");
+            ExcelExportCommand = new RelayCommand(ExecuteExcelExportCommand);
+            WordExportCommand = new RelayCommand(ExecuteWordExportCommand);
         }
+
+        private void ExecuteWordExportCommand()
+        {
+            // Создание нового документа Word
+            XWPFDocument document = new XWPFDocument();
+
+            // Создание таблицы
+            XWPFTable table = document.CreateTable(pets.Count + 1, 8);
+
+            // Заполнение заголовков столбцов
+            table.GetRow(0).GetCell(0).SetText("Name");
+            table.GetRow(0).GetCell(1).SetText("Breed");
+            table.GetRow(0).GetCell(2).SetText("Gender");
+            table.GetRow(0).GetCell(3).SetText("Status");
+            table.GetRow(0).GetCell(4).SetText("Breed");
+            table.GetRow(0).GetCell(5).SetText("Дата рождения");
+            table.GetRow(0).GetCell(6).SetText("Дата появления в котокафе");
+            table.GetRow(0).GetCell(7).SetText("Номер паспорта");
+
+            // Заполнение данных о питомцах
+            for (int i = 0; i < pets.Count; i++)
+            {
+                PetModel pet = pets[i];
+
+                table.GetRow(i + 1).GetCell(0).SetText(pet.Name);
+                table.GetRow(i + 1).GetCell(1).SetText(pet.Breed.Title);
+                table.GetRow(i + 1).GetCell(2).SetText(pet.Gender.Title);
+                table.GetRow(i + 1).GetCell(3).SetText(pet.Status.Title);
+                table.GetRow(i + 1).GetCell(4).SetText(pet.Breed.Title);
+                table.GetRow(i + 1).GetCell(5).SetText(pet.Birthday.ToString());
+                table.GetRow(i + 1).GetCell(6).SetText(pet.CheckInDate.ToString());
+                table.GetRow(i + 1).GetCell(7).SetText(pet.PassNumber);
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Word Documents|*.docx";
+            saveFileDialog.DefaultExt = ".docx";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                using (FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create, FileAccess.Write))
+                {
+
+                    document.Write(stream);
+                }
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = saveFileDialog.FileName,
+                    UseShellExecute = true
+                });
+            }
+        }
+
+        private void ExecuteExcelExportCommand()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.ShowDialog();
+                saveFileDialog.DefaultExt = ".xlsx";
+                var worksheet = workbook.Worksheets.Add("Pets");
+                worksheet.Cell("A1").InsertTable(pets);
+                string path = saveFileDialog.FileName + ".xlsx";
+                workbook.SaveAs(path);
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = path,
+                    UseShellExecute = true
+                });
+            }
+        }
+
         public static ObservableCollection<PetModel> GetPetsFromTable(string table)
         {
             ObservableCollection<PetModel> pets = new ObservableCollection<PetModel>();
